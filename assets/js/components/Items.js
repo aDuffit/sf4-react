a1dbae68dbca77d41e4be7ed12b28fa32e3792bf
aDuffit <a.duffit@yahoo.fr>
2019-06-19 09:37:47 +0200

    first commit

---
+++ assets/js/components/Items.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Items = ({ id, title, body }) => (
+    <div key={id} className="card col-md-4" style={{width:200}}>
+        <div className="card-body">
+            <p>{id}</p>
+            <h4 className="card-title">{title}</h4>
+            <p className="card-text">{body}</p>
+        </div>
+    </div>
+);
+
+export default Items;
